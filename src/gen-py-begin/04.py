2f37e523edd23d2ffd7058341f0cfe08f92f616f
Elena Listratova <int28h@protonmail.com>
2022-06-14 21:12:10 +0300

    (´ ∀ ` *)

--- src/gen-py-begin/04.py
+++ src/gen-py-begin/04.py
@@ -55,4 +55,13 @@ if (two - one) + two == three:
     print('YES')
 else:
     print('NO')
-    
+
+'''
+Напишите программу, которая определяет наименьшее из двух чисел.
+'''
+a = int(input())
+b = int(input())
+if a < b:
+    print(a)
+else:
+    print(b)
