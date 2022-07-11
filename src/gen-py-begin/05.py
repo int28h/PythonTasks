aae97274bb7fb38ab3d8b8b553b031cb9160bd32
Elena Listratova <int28h@protonmail.com>
2022-07-11 21:00:25 +0300

    (*´▽`*)

--- src/gen-py-begin/05.py
+++ src/gen-py-begin/05.py
@@ -55,3 +55,14 @@ print(int(input * 10  % 10))
 '''
 input = float(input())
 print(input % int(input))
+
+'''
+Напишите программу, которая находит наименьшее и наибольшее из пяти чисел.
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+print('Наименьшее число =', min(a, b, c, d, e))
+print('Наибольшее число =', max(a, b, c, d, e))
