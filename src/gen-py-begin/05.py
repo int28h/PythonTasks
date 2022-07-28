09f3961d41eedc40c8a856f52a2c3b83ca2c9f4e
Elena Listratova <int28h@protonmail.com>
2022-07-28 23:25:44 +0300

    \(^ヮ^)/

--- src/gen-py-begin/05.py
+++ src/gen-py-begin/05.py
@@ -142,3 +142,17 @@ two = input()
 three = input()
 print(min(one, two, three, key=len))
 print(max(one, two, three, key=len))
+
+'''
+Вводятся 3 строки в случайном порядке.
+Напишите программу, которая выясняет можно ли из длин этих строк построить
+возрастающую арифметическую прогрессию.
+'''
+a = len(input())
+b = len(input())
+c = len(input())
+
+if (2 * b - c - a) * (2 * c -b - a) * (2 * a -b - c) == 0:
+    print('YES')
+else:
+    print('NO')
